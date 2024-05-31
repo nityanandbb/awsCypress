98d1479a5f700c3bce99ece3cb55e48ccfbbec39
nityanandbb <nityanand.bharati@qed42.com>
2024-05-31 16:54:42 +0530

    report 1

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,18 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // integrate cypress-mochawesome-reporter plugin
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
-    baseUrl:null,
+    baseUrl: null,
     video: false,
     defaultCommandTimeout: 10000,
+  },
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "mochawesome-report",
+    overwrite: false,
+    html: true,
+    json: true,
   },
 });
